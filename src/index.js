4c277955ab3461859d2451adee9201fec25abb12
Brandon Bakus <brandonbakus@gmail.com>
2025-01-13 15:54:08 -0500

    I did it

--- src/index.js
+++ src/index.js
@@ -1,21 +1,67 @@
 // index.js
 
-// Callbacks
+
 const displayBurgerDetails = (burger) => {
-    // Add code
+   
+    const image = document.querySelector('#image')
+    image.src = burger.image
+    const name = document.getElementById('name')
+    name.textContent = burger.name
+    
+    
+};
+
+const addBurgerNamesToMenu = () => {
+    fetch("http://localhost:3000/burgers")
+        .then(response => response.json())
+        .then(burgers => {
+            const menuList = document.querySelector('#restaurant-menu')
+            
+            burgers.forEach(burger => {
+                const span = document.createElement('span')
+                span.textContent = burger.name
+                menuList.appendChild(span)
+                span.style.width = '250px'
+                span.addEventListener('click', () => displayBurgerDetails(burger))
+                
+                if(burgers.length > 1){
+                    displayBurgerDetails(burgers[0])
+                }
+            
+            })
+
+        })
+        
+        
 };
 
 const addToCart = () => {
-    // Add code
+    const addToCartForm = document.querySelector('#add-to-cart-form')
+    let total = 0
+    addToCartForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    const inputNumber = document.getElementById('number-to-add')
+    const numberInCart = document.getElementById('number-in-cart-count')
+    
+    total += parseInt(inputNumber.value)
+    numberInCart.textContent = total
+    addToCartForm.reset()
+    
+    
+    })
 }
-
-const addBurgerNamesToMenu = () => {
-    // Add code
-};
 
 const main = () => {
-    // Invoke addBurgerNamesToMenu here
-    // Invoke addToCart here
+   
+    document.addEventListener('DOMContentLoaded', main => {
+    
+    addBurgerNamesToMenu()
+    addToCart()
+    
+})
 }
 
-main()
+main()  
+
+
+
